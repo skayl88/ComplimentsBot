4812db27d82b4779d369e6023afc0e1a4711ca3c
Evgenii <skayl8827@gmail.com~>
2024-06-05 19:34:21 +0700

    init

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,28 +1,10 @@
-const responses = [
-  "It is certain.",
-  "Without a doubt.",
-  "You may rely on it.",
-  "Yes, definitely.",
-  "It is decidedly so.",
-  "As I see it, yes.",
-  "Most likely.",
-  "Outlook good.",
-  "Signs point to yes.",
-  "Absolutely!",
-  "Don't count on it.",
-  "My sources say no.",
-  "Outlook not so good.",
-  "Very doubtful.",
-  "Not likely.",
-  "Better not tell you now.",
-  "Chances are low.",
-  "Unlikely.",
-  "No way, lol.",
-  "I highly doubt it.",
-  "Maybe....Maybe not?",
-  "Ask again later.",
-];
 
+import axios from 'axios';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const getRandomCompliment = (compliments) => {
+  const randomIndex = Math.floor(Math.random() * compliments.length);
+  return compliments[randomIndex].text;
+};
 export default {
   name: "8ball",
   description: "Ask the magic 8ball a question",
@@ -31,15 +13,13 @@ export default {
   example: "/8ball Is this bot awesome?",
   category: "Fun",
   handler: async (ctx) => {
-    const { message } = ctx;
-    const { text } = message;
-
-    if (text.split(" ").length < 2) {
-      await ctx.reply("You need to ask a question!");
-      return;
+    try {
+      const response = await axios.get('https://backendbottg-production.up.railway.app/compliments/');
+      const compliments = response.data;
+      const randomCompliment = getRandomCompliment(compliments);
+      ctx.reply(` ${randomCompliment}`);
+    } catch (error) {
+      ctx.reply('Произошла ошибка при получении комплимента.');
     }
-
-    const response = responses[Math.floor(Math.random() * responses.length)];
-    await ctx.reply(response);
-  },
-};
+  }
+}
